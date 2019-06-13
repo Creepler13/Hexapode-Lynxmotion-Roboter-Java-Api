88e37dba20f935957e4723dbe5414d90af983faa
Paul Meier <denis.schlichting03@gmail.com>
2019-06-13 21:54:19 +0200

    Librarys added

--- Hexapode Java Api/src/Hexapode.java
+++ Hexapode Java Api/src/Hexapode.java
@@ -1,49 +1,57 @@
 import java.io.IOException;
 
 import com.pi4j.io.gpio.exception.UnsupportedBoardType;
-import com.pi4j.io.serial.*;
+import com.pi4j.io.serial.Baud;
+import com.pi4j.io.serial.DataBits;
+import com.pi4j.io.serial.FlowControl;
+import com.pi4j.io.serial.Parity;
+import com.pi4j.io.serial.Serial;
+import com.pi4j.io.serial.SerialConfig;
+import com.pi4j.io.serial.SerialDataEvent;
+import com.pi4j.io.serial.SerialDataEventListener;
+import com.pi4j.io.serial.SerialFactory;
+import com.pi4j.io.serial.SerialPort;
+import com.pi4j.io.serial.StopBits;
 
 public class Hexapode {
 	/**
 	 * @param args
 	 */
 	final static Serial serial = SerialFactory.createInstance();
-	
-	public static void start()  {
+
+	public static void start() {
 		serial.addListener(new SerialDataEventListener() {
 			public void dataReceived(SerialDataEvent event) {
 
 			}
 		});
 		SerialConfig config = new SerialConfig();
-		try{
+		try {
 			config.device(SerialPort.getDefaultPort()).baud(Baud._38400).dataBits(DataBits._8).parity(Parity.NONE)
 					.stopBits(StopBits._1).flowControl(FlowControl.NONE);
 		} catch (UnsupportedBoardType | IOException | InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-			try {
-				serial.open(config);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-			
+
+		try {
+			serial.open(config);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	
-	
+
+	}
+
 	public static void home() {
-		
+
 		try {
 			serial.write("#5 P1600 #10 P750 T2500 <cr>");
 		} catch (IllegalStateException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 }
