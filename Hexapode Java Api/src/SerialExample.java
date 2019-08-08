15fdcab51a90a07c0d8c213b1c7cc81aa78f3566
Creepler13 <denis.schlichting03@gmail.com>
2019-08-08 15:06:18 +0200

    test

--- Hexapode Java Api/src/SerialExample.java
+++ Hexapode Java Api/src/SerialExample.java
@@ -28,123 +28,75 @@
  * #L%
  */
 
-
-import com.pi4j.io.serial.*;
-import com.pi4j.util.CommandArgumentParser;
-import com.pi4j.util.Console;
-
 import java.io.IOException;
-import java.util.Date;
+
+import com.pi4j.io.serial.Baud;
+import com.pi4j.io.serial.DataBits;
+import com.pi4j.io.serial.FlowControl;
+import com.pi4j.io.serial.Parity;
+import com.pi4j.io.serial.Serial;
+import com.pi4j.io.serial.SerialConfig;
+import com.pi4j.io.serial.SerialFactory;
+import com.pi4j.io.serial.SerialPort;
+import com.pi4j.io.serial.StopBits;
+import com.pi4j.util.CommandArgumentParser;
 
 /**
- * This example code demonstrates how to perform serial communications using the Raspberry Pi.
+ * This example code demonstrates how to perform serial communications using the
+ * Raspberry Pi.
  *
  * @author Robert Savage
  */
 public class SerialExample {
 
-    /**
-     * This example program supports the following optional command arguments/options:
-     *   "--device (device-path)"                   [DEFAULT: /dev/ttyAMA0]
-     *   "--baud (baud-rate)"                       [DEFAULT: 38400]
-     *   "--data-bits (5|6|7|8)"                    [DEFAULT: 8]
-     *   "--parity (none|odd|even)"                 [DEFAULT: none]
-     *   "--stop-bits (1|2)"                        [DEFAULT: 1]
-     *   "--flow-control (none|hardware|software)"  [DEFAULT: none]
-     *
-     * @param args
-     * @throws InterruptedException
-     * @throws IOException
-     */
-    public static void main(String args[]) throws InterruptedException, IOException {
+	/**
+	 * This example program supports the following optional command
+	 * arguments/options: "--device (device-path)" [DEFAULT: /dev/ttyAMA0] "--baud
+	 * (baud-rate)" [DEFAULT: 38400] "--data-bits (5|6|7|8)" [DEFAULT: 8] "--parity
+	 * (none|odd|even)" [DEFAULT: none] "--stop-bits (1|2)" [DEFAULT: 1]
+	 * "--flow-control (none|hardware|software)" [DEFAULT: none]
+	 *
+	 * @param args
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	public static void main(String args[]) throws InterruptedException, IOException {
 
-        // !! ATTENTION !!
-        // By default, the serial port is configured as a console port
-        // for interacting with the Linux OS shell.  If you want to use
-        // the serial port in a software program, you must disable the
-        // OS from using this port.
-        //
-        // Please see this blog article for instructions on how to disable
-        // the OS console for this port:
-        // https://www.cube-controls.com/2015/11/02/disable-serial-port-terminal-output-on-raspbian/
+		// create an instance of the serial communications class
+		final Serial serial = SerialFactory.createInstance();
 
-        // create Pi4J console wrapper/helper
-        // (This is a utility class to abstract some of the boilerplate code)
-        final Console console = new Console();
+		try {
+			// create serial config object
+			SerialConfig config = new SerialConfig();
 
-        // print program title/header
-        console.title("<-- The Pi4J Project -->", "Serial Communication Example");
+			// set default serial settings (device, baud rate, flow control, etc)
+			//
+			// by default, use the DEFAULT com port on the Raspberry Pi (exposed on GPIO
+			// header)
+			// NOTE: this utility method will determine the default serial port for the //
+			// except the 3B, it will return "/dev/ttyAMA0". For Raspberry Pi
+			// model 3B may return "/dev/ttyS0" or "/dev/ttyAMA0" depending on
+			// environment configuration.
+			config.device(SerialPort.getDefaultPort()).baud(Baud._9600).dataBits(DataBits._8).parity(Parity.NONE)
+					.stopBits(StopBits._1).flowControl(FlowControl.NONE);
 
-        // allow for user to exit program using CTRL-C
-        console.promptForExit();
+			// parse optional command argument options to override the default serial
+			// settings.
+			if (args.length > 0) {
+				config = CommandArgumentParser.getSerialConfig(config, args);
+			}
 
-        // create an instance of the serial communications class
-        final Serial serial = SerialFactory.createInstance();
+			// #Se5 P1600 #10 P750 T2500 <cr> open the default serial device/port with the
+			// configuration settings
+			serial.open(config);
 
-           try {
-            // create serial config object
-            SerialConfig config = new SerialConfig();
+			serial.write("#5 P1000 #10 P2000 T2500 <cr>");
+			// continuous loop to keep the program running until the user terminates the
+			// program
 
-            // set default serial settings (device, baud rate, flow control, etc)
-            //
-            // by default, use the DEFAULT com port on the Raspberry Pi (exposed on GPIO header)
-            // NOTE: this utility method will determine the default serial port for the       //       except the 3B, it will return "/dev/ttyAMA0".  For Raspberry Pi
-            //       model 3B may return "/dev/ttyS0" or "/dev/ttyAMA0" depending on
-            //       environment configuration.
-            config.device(SerialPort.getDefaultPort())
-                  .baud(Baud._9600)
-                  .dataBits(DataBits._8)
-                  .parity(Parity.NONE)
-                  .stopBits(StopBits._1)
-                  .flowControl(FlowControl.NONE);
+		} catch (IOException ex) {
 
-            // parse optional command argument options to override the default serial settings.
-            if(args.length > 0){
-                config = CommandArgumentParser.getSerialConfig(config, args);
-            }
-
-            // display connection details
-            console.box(" Connecting to: " + config.toString(),
-                    " We are sending ASCII data on the serial port every 1 second.",
-                    " Data received on serial port will be displayed below.");
-
-
-            //#Se5 P1600 #10 P750 T2500 <cr> open the default serial device/port with the configuration settings
-            serial.open(config);
-
-            serial.write("#5 P1000 #10 P2000 T2500 <cr>");
-           // continuous loop to keep the program running until the user terminates the program
-            while(console.isRunning()) {
-                try {
-                    // write a formatted string to the serial transmit buffer
-                    serial.write("CURRENT TIME: " + new Date().toString());
-
-                    // write a individual bytes to the serial transmit buffer
-                   serial.write((byte) 13);
-                    serial.write((byte) 10);
-
-                    // write a simple string to the serial transmit buffer
-                    serial.write("Second Line");
-
-                    // write a individual characters to the serial transmit buffer
-                    serial.write('\r');
-                    serial.write('\n');
-
-                    // write a string terminating with CR+LF to the serial transmit buffer
-                    serial.writeln("Third Line");
-                }
-                catch(IllegalStateException ex){
-                    ex.printStackTrace();
-                }
-
-                // wait 1 second before continuing
-                Thread.sleep(1000);
-            }
-
-        }
-        catch(IOException ex) {
-            console.println(" ==>> SERIAL SETUP FAILED : " + ex.getMessage());
-            return;
-        }
-    }
+			return;
+		}
+	}
 }
