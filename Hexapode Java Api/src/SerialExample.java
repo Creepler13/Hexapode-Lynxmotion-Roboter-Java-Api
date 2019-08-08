63aff981beaaab6cd38b40f423ee69b3ffbda25e
Creepler13 <denis.schlichting03@gmail.com>
2019-08-08 15:10:24 +0200

    test

--- Hexapode Java Api/src/SerialExample.java
+++ Hexapode Java Api/src/SerialExample.java
@@ -28,75 +28,81 @@
  * #L%
  */
 
-import java.io.IOException;
 
-import com.pi4j.io.serial.Baud;
-import com.pi4j.io.serial.DataBits;
-import com.pi4j.io.serial.FlowControl;
-import com.pi4j.io.serial.Parity;
-import com.pi4j.io.serial.Serial;
-import com.pi4j.io.serial.SerialConfig;
-import com.pi4j.io.serial.SerialFactory;
-import com.pi4j.io.serial.SerialPort;
-import com.pi4j.io.serial.StopBits;
+import com.pi4j.io.serial.*;
 import com.pi4j.util.CommandArgumentParser;
+import com.pi4j.util.Console;
+
+import java.io.IOException;
+import java.util.Date;
 
 /**
- * This example code demonstrates how to perform serial communications using the
- * Raspberry Pi.
+ * This example code demonstrates how to perform serial communications using the Raspberry Pi.
  *
  * @author Robert Savage
  */
 public class SerialExample {
 
-	/**
-	 * This example program supports the following optional command
-	 * arguments/options: "--device (device-path)" [DEFAULT: /dev/ttyAMA0] "--baud
-	 * (baud-rate)" [DEFAULT: 38400] "--data-bits (5|6|7|8)" [DEFAULT: 8] "--parity
-	 * (none|odd|even)" [DEFAULT: none] "--stop-bits (1|2)" [DEFAULT: 1]
-	 * "--flow-control (none|hardware|software)" [DEFAULT: none]
-	 *
-	 * @param args
-	 * @throws InterruptedException
-	 * @throws IOException
-	 */
-	public static void main(String args[]) throws InterruptedException, IOException {
+    /**
+     * This example program supports the following optional command arguments/options:
+     *   "--device (device-path)"                   [DEFAULT: /dev/ttyAMA0]
+     *   "--baud (baud-rate)"                       [DEFAULT: 38400]
+     *   "--data-bits (5|6|7|8)"                    [DEFAULT: 8]
+     *   "--parity (none|odd|even)"                 [DEFAULT: none]
+     *   "--stop-bits (1|2)"                        [DEFAULT: 1]
+     *   "--flow-control (none|hardware|software)"  [DEFAULT: none]
+     *
+     * @param args
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    public static void main(String args[]) throws InterruptedException, IOException {
 
-		// create an instance of the serial communications class
-		final Serial serial = SerialFactory.createInstance();
+        // create an instance of the serial communications class
+        final Serial serial = SerialFactory.createInstance();
 
-		try {
-			// create serial config object
-			SerialConfig config = new SerialConfig();
+           try {
+            // create serial config object
+            SerialConfig config = new SerialConfig();
 
-			// set default serial settings (device, baud rate, flow control, etc)
-			//
-			// by default, use the DEFAULT com port on the Raspberry Pi (exposed on GPIO
-			// header)
-			// NOTE: this utility method will determine the default serial port for the //
-			// except the 3B, it will return "/dev/ttyAMA0". For Raspberry Pi
-			// model 3B may return "/dev/ttyS0" or "/dev/ttyAMA0" depending on
-			// environment configuration.
-			config.device(SerialPort.getDefaultPort()).baud(Baud._9600).dataBits(DataBits._8).parity(Parity.NONE)
-					.stopBits(StopBits._1).flowControl(FlowControl.NONE);
+            // set default serial settings (device, baud rate, flow control, etc)
+            //
+            // by default, use the DEFAULT com port on the Raspberry Pi (exposed on GPIO header)
+            // NOTE: this utility method will determine the default serial port for the       //       except the 3B, it will return "/dev/ttyAMA0".  For Raspberry Pi
+            //       model 3B may return "/dev/ttyS0" or "/dev/ttyAMA0" depending on
+            //       environment configuration.
+            config.device(SerialPort.getDefaultPort())
+                  .baud(Baud._9600)
+                  .dataBits(DataBits._8)
+                  .parity(Parity.NONE)
+                  .stopBits(StopBits._1)
+                  .flowControl(FlowControl.NONE);
 
-			// parse optional command argument options to override the default serial
-			// settings.
-			if (args.length > 0) {
-				config = CommandArgumentParser.getSerialConfig(config, args);
-			}
+            // parse optional command argument options to override the default serial settings.
+            if(args.length > 0){
+                config = CommandArgumentParser.getSerialConfig(config, args);
+            }
 
-			// #Se5 P1600 #10 P750 T2500 <cr> open the default serial device/port with the
-			// configuration settings
-			serial.open(config);
+      
 
-			serial.write("#5 P1000 #10 P2000 T2500 <cr>");
-			// continuous loop to keep the program running until the user terminates the
-			// program
+            //#Se5 P1600 #10 P750 T2500 <cr> open the default serial device/port with the configuration settings
+            serial.open(config);
 
-		} catch (IOException ex) {
+            serial.write("#5 P1000 #10 P2000 T2500 <cr>");
+           // continuous loop to keep the program running until the user terminates the program
+           for (int i = 0; i < 100; i++) {
+			
+		
+              
+            
 
-			return;
-		}
-	}
+    
+                Thread.sleep(1000);
+            }
+
+        }
+        catch(IOException ex) {
+            return;
+        }
+    }
 }
