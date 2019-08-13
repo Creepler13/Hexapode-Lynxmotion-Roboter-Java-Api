9495341d976ac16f45312fc26cf81fdf10312ca2
Creepler13 <denis.schlichting03@gmail.com>
2019-08-13 14:36:46 +0200

    tests

--- Hexapode Java Api/src/Start.java
+++ Hexapode Java Api/src/Start.java
@@ -4,9 +4,9 @@ public class Start {
 	public static void main(String args[]) {
 //		
 		
-		Hexapode.debug("tet");
-		//Hexapode.start(0,args);
-//		Hexapode.home();
+		//Hexapode.debug("tet");
+		Hexapode.start(0,args);
+		Hexapode.home();
 //		Hexapode.action("println/Print/");
 	}
 
