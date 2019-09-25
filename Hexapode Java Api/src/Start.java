56d7a3dfae7cff378a4725cf949903827b0da5da
Creepler13 <denis.schlichting03@gmail.com>
2019-09-25 15:12:48 +0200

    up start

--- Hexapode Java Api/src/Start.java
+++ Hexapode Java Api/src/Start.java
@@ -1,9 +1,10 @@
+import api.basic.Hexapode;
 
 public class Start {
 
 	public static void main(String args[]) {
-		Hexapode hexa = new Hexapode(Baud._9600);
-		Hexapode.moveServo(13, 1500, 1500, false);
+		Hexapode hexa = new Hexapode();
+		
 	}
 
 }
--- Hexapode Java Api/src/api/basic/Hexapode.java
+++ Hexapode Java Api/src/api/basic/Hexapode.java
@@ -1,3 +1,5 @@
+package api.basic;
+
 import java.io.IOException;
 
 import com.pi4j.io.gpio.exception.UnsupportedBoardType;
@@ -19,10 +21,10 @@ public class Hexapode {
 	final static Serial serial = SerialFactory.createInstance();
 	final static Console console = new Console();
 
-	public Hexapode(Baud baud) {
+	public Hexapode() {
 		try {
 			SerialConfig config = new SerialConfig();
-			config.device(SerialPort.getDefaultPort()).baud(baud).dataBits(DataBits._8).parity(Parity.NONE)
+			config.device(SerialPort.getDefaultPort()).baud(Baud._9600).dataBits(DataBits._8).parity(Parity.NONE)
 					.stopBits(StopBits._1).flowControl(FlowControl.NONE);
 			serial.addListener(new SerialDataEventListener() {
 				@Override
@@ -35,38 +37,12 @@ public class Hexapode {
 		}
 	}
 
-	/**
-	 * jkjkljk.
-	 * 
-	 * @param servo      = Servo Controller Motor pin Nummer
-	 *
-	 * @param state      = gewünschte Servo position bsp: 1500
-	 * 
-	 * @param time       = Zeit bis der Servo state erreicht in ms
-	 * 
-	 * @param stopThread = true = stoppt den Main Thread für die zeit die der Servo
-	 *                   bis [state] braucht([time]), false = motor bewegt sich
-	 *                   thread läuft weiter.
-	 * @return null
-	 */
-
-	public static void moveServo(int servo, int state, int time, boolean stopThread) {
-		try {
-			if (stopThread) {
-				Thread.sleep(time);
-			}
-			serial.write("#" + servo + " P" + state + " T" + time + " <cr>");
-		} catch (IllegalStateException | IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void serialcommand(String cmd) {
+	try {
+		serial.write(cmd + "\r");
+	} catch (IllegalStateException | IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
-
-	/**
-	 * 
-	 * @param msg String der in der Console aus
-	 */
-	public static void println(String msg) {
-		console.println(msg);
 	}
 }
