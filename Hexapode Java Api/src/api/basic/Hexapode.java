c7f8a14f17bf4af15f6fe3a00a5891038e288323
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2019-10-22 23:00:29 +0200

    Implemented PIN-mapping and added documentation (Javadoc comments)
    - documented Hexapode.java and Util.java
    - PIN-mapping can be used by invoking the exec method of the Hexapode
    instance

--- Hexapode Java Api/src/api/basic/Hexapode.java
+++ Hexapode Java Api/src/api/basic/Hexapode.java
@@ -16,17 +16,40 @@ import com.pi4j.io.serial.SerialPort;
 import com.pi4j.io.serial.StopBits;
 import com.pi4j.util.Console;
 
+/**
+ * 
+ * @author Creepler13 & JustAnotherJavaProgrammer
+ *
+ *         Lowest-level class to control the hexapod directly by sending
+ *         commands.
+ *         <p>
+ *         This is the base class used by all classes of this API to access the
+ *         hardware. <br>
+ *         There may only be one shared instance of this class. You can get it
+ *         by calling {@link api.basic.Hexapode#getInstance() getInstance()} on
+ *         this class.
+ */
 public class Hexapode {
 
-	final static Serial serial = SerialFactory.createInstance();
-	final static Console console = new Console();
+	private final static Serial serial = SerialFactory.createInstance();
+	
+	// For future use
+	private final static Console console = new Console();
+
+	// WARNING: The PINS must not have IDs below 0!
+	public static final int[][] PIN_MAPPING = new int[0][0];
 
 	private static final Hexapode instance = new Hexapode();
-	
+
+	/**
+	 * Get the shared instance of this Singleton
+	 * 
+	 * @return An instance of this class
+	 */
 	public static Hexapode getInstance() {
 		return instance;
 	}
-	
+
 	private Hexapode() {
 		try {
 			SerialConfig config = new SerialConfig();
@@ -43,12 +66,38 @@ public class Hexapode {
 		}
 	}
 
+	/**
+	 * Directly executes a command.
+	 * 
+	 * <p>
+	 * Note: This method does not use the PIN-mapping {@link}
+	 * 
+	 * @param cmd The command to be executed
+	 * @see api.basic.Hexapode#exec Execute a command with applied PIN-mapping
+	 */
 	public void serialCommand(String cmd) {
-	try {
-		serial.write(cmd + "\r");
-	} catch (IllegalStateException | IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		try {
+			serial.write(cmd + "\r");
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+		}
 	}
+
+	/**
+	 * Execute a command and apply PIN-mapping
+	 *
+	 * @param command The command to be executed
+	 */
+	public void exec(String command) {
+		command.replaceAll(" ", "");
+		for (int i = 0; i < PIN_MAPPING.length; i++) {
+			command.replaceAll("#" + PIN_MAPPING[i][0], "#" + (-PIN_MAPPING[i][1]));
+		}
+		for (int i = 0; i < PIN_MAPPING.length; i++) {
+			command.replaceAll("#" + (-PIN_MAPPING[i][1]), "#" + PIN_MAPPING[i][1]);
+		}
+		serialCommand(command);
 	}
+	
+	// TODO Write to console
 }
