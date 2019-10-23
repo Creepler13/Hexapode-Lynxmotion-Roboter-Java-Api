4821f1fb976ad6f2c7719cf158552747632dce7e
fsglab-elec <fsglab-elec@fsg-lab-6>
2019-10-23 12:57:51 +0200

    Does it work now?

--- Hexapode Java Api/src/api/basic/Hexapode.java
+++ Hexapode Java Api/src/api/basic/Hexapode.java
@@ -93,6 +93,7 @@ public class Hexapode {
 		}
 		command.replaceAll(" ", "");
 		for (int i = 0; i < PIN_MAPPING.length; i++) {
+			System.out.println("Replace\n"+"#" + PIN_MAPPING[i][1]+"\nby\n\"#\" + (-PIN_MAPPING[i][0])");
 			command.replaceAll("#" + PIN_MAPPING[i][1], "#" + (-PIN_MAPPING[i][0]));
 		}
 		for (int i = 0; i < PIN_MAPPING.length; i++) {
