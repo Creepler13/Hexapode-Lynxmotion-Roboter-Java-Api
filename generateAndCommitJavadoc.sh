ba880e8da879d7f332567d72be22617db183c42c
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2019-10-26 20:39:54 +0200

    Regenerated the Javadoc

--- generateAndCommitJavadoc.sh
+++ generateAndCommitJavadoc.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+args=("$@")
 
+function goto
+{
+label=$1
+cmd=$(sed -n "/$label:/{:a;n;p;ba};" $0 | grep -v ':$')
+eval "$cmd"
+exit
+}
+
+echo off
 echo Generating Javadoc...
-javadoc -use -classpath Hexapode\ Java\ API/lib/* -sourcepath Hexapode\ Java Api/src --allow-script-in-comments -d docs -use -windowtitle Hexapod\ Lynxmotion\ API\ Documentation" -header Lynxmotion\ Hexapod\ Java\ API\ (unofficial\ API) -J-Xmx512m -subpackages api test
+javadoc -use -classpath "Hexapode Java API\lib\*" -sourcepath "Hexapode Java Api\src" --allow-script-in-comments -d docs -use -windowtitle "Hexapod Lynxmotion API Documentation" -header "Lynxmotion Hexapod Java API (unofficial API)" -overview "Hexapode Java Api\src\overview.html" -J-Xmx512m -subpackages api test
+sleep 
 echo Adding newly generated doc files...
 git add -A docs
 echo Committing...
-git commit -a -m Regenerated\ the\ Javadoc
+git commit -a -m "Regenerated the Javadoc"
 echo Pushing...
 git push
 echo Finished!
+sleep
