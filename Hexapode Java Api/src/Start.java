a9debae85a0dc75c98431e0359542f6c4ff57734
fsglab-elec <fsglab-elec@fsg-lab-5>
2019-11-27 16:21:20 +0100

    Added a template for RoboScript
    - Trying to make the robot walk

--- Hexapode Java Api/src/Start.java
+++ Hexapode Java Api/src/Start.java
@@ -2,7 +2,9 @@ import java.io.IOException;
 
 import api.advanced.Bundle;
 import api.advanced.BundleCreator;
+import api.advanced.Collection;
 import api.basic.Hexapode;
+import static api.basic.PINConstants.*;
 
 public class Start {
 
@@ -13,18 +15,98 @@ public class Start {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		Bundle bundle = BundleCreator.moveAllLegs(750, 1000, 2300, 2000);
-		Bundle bundle2 = BundleCreator.moveAllLegs(750, 0, 750, 2000);
+//		Bundle bundle = BundleCreator.moveAllLegs(750, 1000, 2300, 2000);
+//		Bundle bundle2 = BundleCreator.moveAllLegs(750, 0, 750, 2000);
 //		Bundle bundle2 = new Bundle(2000);
 //		bundle2.add(302, 1500);
 
 //		bundle.add(bundle2);
-		while (true) {
-			bundle.execBlocking();
-			bundle2.execBlocking();
-		}
+//		while (true) {
+//			bundle.execBlocking();
+//			bundle2.execBlocking();
+//		}
 
 //		System.out.println(Hexapode.getClient().applyPositionMapping("#210P1300T560"));
+
+		Bundle leftA = new Bundle(250);
+		Bundle leftB = new Bundle(250);
+		Bundle leftC = new Bundle(250);
+		Bundle leftD = new Bundle(250);
+		for (int i = 0; i < 3; i++) {
+			int leg = 0;
+			switch (i) {
+			case 0:
+				leg = FRONT;
+				break;
+			case 1:
+				leg = MIDDLE;
+				break;
+			default:
+				leg = BACK;
+				break;
+			}
+			if (i == 1) {
+				leftA.add(BundleCreator.moveLeg(leg + LEFT, 1500, 500, 1000, 250));
+				leftB.add(BundleCreator.moveLeg(leg + LEFT, 1500, 750, 1575, 250));
+				leftC.add(BundleCreator.moveLeg(leg + LEFT, 0, 750, 1575, 250));
+				leftD.add(BundleCreator.moveLeg(leg + LEFT, 0, 500, 1000, 250));
+			} else {
+				leftA.add(BundleCreator.moveLeg(leg + LEFT, 0, 750, 1575, 250));
+				leftB.add(BundleCreator.moveLeg(leg + LEFT, 0, 500, 1000, 250));
+				leftC.add(BundleCreator.moveLeg(leg + LEFT, 1500, 500, 1000, 250));
+				leftD.add(BundleCreator.moveLeg(leg + LEFT, 1500, 750, 1575, 250));
+			}
+
+		}
+
+		Bundle rightA = new Bundle(250);
+		Bundle rightB = new Bundle(250);
+		Bundle rightC = new Bundle(250);
+		Bundle rightD = new Bundle(250);
+		for (int i = 0; i < 3; i++) {
+			int leg = 0;
+			switch (i) {
+			case 0:
+				leg = FRONT;
+				break;
+			case 1:
+				leg = MIDDLE;
+				break;
+			default:
+				leg = BACK;
+				break;
+			}
+			if (i == 1) {
+				rightA.add(BundleCreator.moveLeg(leg + RIGHT, 1500, 500, 1000, 250));
+				rightB.add(BundleCreator.moveLeg(leg + RIGHT, 1500, 750, 1575, 250));
+				rightC.add(BundleCreator.moveLeg(leg + RIGHT, 0, 750, 1575, 250));
+				rightD.add(BundleCreator.moveLeg(leg + RIGHT, 0, 500, 1000, 250));
+			} else {
+				rightA.add(BundleCreator.moveLeg(leg + RIGHT, 0, 1000, 1575, 250));
+				rightB.add(BundleCreator.moveLeg(leg + RIGHT, 0, 1500, 1000, 250));
+				rightC.add(BundleCreator.moveLeg(leg + RIGHT, 1500, 1500, 1000, 250));
+				rightD.add(BundleCreator.moveLeg(leg + RIGHT, 1500, 1000, 1575, 250));
+			}
+		}
+		Collection a = new Collection();
+		a.add(leftA);
+		a.add(rightA);
+		Collection b = new Collection();
+		b.add(leftB);
+		b.add(rightB);
+		Collection c = new Collection();
+		c.add(leftC);
+		c.add(rightC);
+		Collection d = new Collection();
+		d.add(leftD);
+		d.add(rightD);
+		for (int i = 0; i < 20; i++) {
+			a.execBlocking();
+			b.execBlocking();
+			c.execBlocking();
+			d.execBlocking();
+		}
+		System.out.println("Done!");
 	}
 
 }
---
+++ Hexapode Java Api/src/dev/roboscript/Interpreter.java
@@ -0,0 +1,5 @@
+package dev.roboscript;
+
+public class Interpreter {
+
+}
