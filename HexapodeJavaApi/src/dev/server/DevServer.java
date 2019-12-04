5f97514ba2876a92bce8ed0e3f99dbde5b045ebf
fsglab-elec <fsglab-elec@fsg-lab-5>
2019-12-04 14:10:51 +0100

    a

--- HexapodeJavaApi/src/dev/server/DevServer.java
+++ HexapodeJavaApi/src/dev/server/DevServer.java
@@ -1,13 +1,9 @@
 package dev.server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.SocketException;
 import java.util.Collections;
 import java.util.Enumeration;
@@ -38,6 +34,7 @@ public class DevServer extends WebSocketServer {
 
 	private DevServer() {
 		super(new InetSocketAddress(PORT));
+		run();
 	}
 
 	/**
